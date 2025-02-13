c4f6ee32aa3fe9108bf6a5177e73b33330524480
Bhawneet Singh <136002853+Bhawneet1@users.noreply.github.com>
2025-02-13 19:21:36 +0530

    Add files via upload

---
+++ Pair.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  pair<int,char>A(4,'t');
+  cout<< A.first<<endl;
+  cout<< A.second<<endl;
+
+  pair<int,char>B = make_pair(-1,'A');
+  cout<< B.first<<endl;
+  cout<< B.second<<endl;
+}
